41adef66f558894c1eb600517adca70adc6ce2db
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2018-11-15 17:03:01 +0100

    added API version layer

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,7 @@
 const paginate = require('express-paginate');
+
+const routes = require('../routes');
+
 const PORT = 8000;
 const DEFAULT_LIMIT = 10;
 const MAX_LIMIT = 100;
@@ -8,8 +11,10 @@ const app = require('express')();
 app.use(paginate.middleware(DEFAULT_LIMIT, MAX_LIMIT));
 
 app.get('/', (_, res) => res.json({ 'api types': ['rest'] }));
+app.get('/rest', (_, res) => res.json({ 'api versions': ['v1', 'current'] }));
 
-app.use('/rest', require('../routes'));
+app.use('/rest/v1', routes);
+app.use('/rest/current', routes);
 
 module.exports = {
   app,
