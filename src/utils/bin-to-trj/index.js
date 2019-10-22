0bed543be3bd10bb8a3de015c1ae37774e114f61
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-10-22 15:12:58 +0200

    speed optimisation bin-to-trj transform stream

--- src/utils/bin-to-trj/index.js
+++ src/utils/bin-to-trj/index.js
@@ -6,11 +6,11 @@ module.exports = function() {
     transform(chunk, _encoding, next) {
       let output = '';
       for (let index = 0; index < chunk.length; index += 4) {
-        const value = Math.round(chunk.readFloatLE(index) * 1000) / 1000;
-        const [left, right = ''] = value.toString(10).split('.');
-        output += `${left.padStart(4, ' ')}.${right.padEnd(3, '0')}${
-          ++globalCount % 10 ? '' : '\n'
-        }`;
+        const value = chunk.readFloatLE(index);
+        const [left, right] = value.toFixed(3).split('.');
+        const paddedLeft = left.padStart(4, ' ');
+        const end = ++globalCount % 10 ? '' : '\n';
+        output += `${paddedLeft}.${right}${end}`;
       }
       const canContinue = this.push(output, 'ascii');
       if (canContinue) {
