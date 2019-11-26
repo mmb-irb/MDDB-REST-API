75cb4579df7a1fbad1345e3090b38ea92333479c
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-11-26 15:33:34 +0100

    simplify code

--- src/routes/projects/chains/index.js
+++ src/routes/projects/chains/index.js
@@ -3,7 +3,7 @@ const Router = require('express').Router;
 const handler = require('../../../utils/generic-handler');
 
 const { NOT_FOUND } = require('../../../utils/status-codes');
-// Mongo DB filter that only returns published results when the environment is set as "production" 
+// Mongo DB filter that only returns published results when the environment is set as "production"
 const publishedFilter = require('../../../utils/published-filter');
 // Adds the project associated ID from mongo db to the provided object
 const augmentFilterWithIDOrAccession = require('../../../utils/augment-filter-with-id-or-accession');
@@ -36,7 +36,7 @@ module.exports = (_, { projects, chains }) => {
     }),
   );
 
-   // When there is a chain parameter (e.g. .../chains/A)
+  // When there is a chain parameter (e.g. .../chains/A)
   chainRouter.route('/:chain').get(
     handler({
       async retriever(request) {
@@ -64,9 +64,8 @@ module.exports = (_, { projects, chains }) => {
       },
       // If there is retrieved and the retrieved has a sequece, send the analyses in the body
       body(response, retrieved) {
-        if (retrieved && retrieved.sequence){
-          const { value, ...data } = retrieved;
-          response.json({ ...data, ...value });
+        if (retrieved && retrieved.sequence) {
+          response.json(retrieved);
         }
       },
     }),
