f7ca554a8048478c4a24eabcb14e4d5beb4218cc
Dani Beltrán <daniel.beltran@irbbarcelona.org>
2020-08-07 11:34:52 +0200

    Improved mongo memory

--- test-helpers/mongo/index.js
+++ test-helpers/mongo/index.js
@@ -13,41 +13,21 @@ const project1 = {
     ATOMS: 123,
   },
 };
-const project2 = {
-  accession: 'PRUEBA02',
-  published: true,
-  metadata: {
-    NAME: 'prueba 2',
-    UNIT: 'A',
-    ATOMS: 456,
-  },
-};
-const project3 = {
-  accession: 'PRUEBA03',
-  published: false,
-  metadata: {
-    NAME: 'prueba 3',
-    UNIT: 'B',
-    ATOMS: 123,
-  },
-};
 
-const project4 = {
-  published: false,
-  metadata: {
-    NAME: 'prueba 3',
-    UNIT: 'C',
-  },
-};
+const project2 = require('./project.json');
 
 // Set up the fake server and return an available connection to this server
 const establishFakeConnection = async () => {
   let client;
   try {
-    // Create the server
-    const mongod = new MongoMemoryServer();
+    // Create the server with a stablished port and dbname, so the connection string is always the same
+    const mongod = new MongoMemoryServer({
+      instance: { port: 38279, dbName: 'f97aa129-34f0-441b-bbe3-7d9e3750bea0' },
+    });
     const connectionString = await mongod.getConnectionString();
-    client = await mongodb.MongoClient.connect(connectionString);
+    client = await mongodb.MongoClient.connect(connectionString, {
+      useUnifiedTopology: true,
+    });
     //console.log(mongod.getInstanceInfo());
     // Add data to the server to simulate the MoDEL structure
     const db = client.db(process.env.DB_NAME);
@@ -55,8 +35,6 @@ const establishFakeConnection = async () => {
     //console.log(projects);
     await projects.insertOne(project1);
     await projects.insertOne(project2);
-    await projects.insertOne(project3);
-    await projects.insertOne(project4);
     return client;
   } catch (error) {
     console.error('fake mongodb connection error');
