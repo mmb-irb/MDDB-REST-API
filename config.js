d8666e6a28f82bb17f7b13549f34f6cd6f8b2272
Dani Beltrán <daniel.beltran@irbbarcelona.org>
2022-09-16 15:45:30 +0200

    updated config + no longer git ignored

---
+++ config.js
@@ -0,0 +1,30 @@
+// Set the configuration of some elements when the .env is too limited
+// i.e. anything which is not a plain string
+module.exports = {
+  hosts: {
+    'mdposit-dev.bsc.es': {
+      name: 'MDposit',
+      description: 'The main server including all simulations',
+      prefix: 'MDP',
+      collection: null,
+    },
+    'bioexcel-cv19.bsc.es': {
+      name: 'BioExcel-CV19',
+      description: 'The Covid-19 server',
+      prefix: 'MCV19',
+      collection: 'cv19',
+    },
+    'bioexcel-cv19-dev.bsc.es': {
+      name: 'BioExcel-CV19',
+      description: 'The Covid-19 server',
+      prefix: 'MCV19',
+      collection: 'cv19',
+    },
+    'model-cns-dev.bsc.es': {
+      name: 'MoDEL-CNS',
+      description: 'The Central Nervous System server',
+      prefix: 'MCNS',
+      collection: 'mcns',
+    },
+  },
+};
