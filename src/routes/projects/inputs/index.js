402ae10e829e6af794e440d7a5388ebd50b6985e
dbeltran <daniel.beltran@irbbarcelona.org>
2023-07-07 17:09:40 +0200

    updated inputs endpoint

--- src/routes/projects/inputs/index.js
+++ src/routes/projects/inputs/index.js
@@ -32,58 +32,61 @@ module.exports = (_, { projects }) => {
       },
       // If there is retrieved and the retrieved has metadata then send the inputs file
       body(response, retrieved) {
-        if (retrieved) {
-          const metadata = retrieved.metadata;
-          // Prepare the input interactions as only interaction names and selections
-          const interactions = metadata.INTERACTIONS;
-          if (interactions) {
-            for (const interaction of interactions) {
-              delete interaction.residues_1;
-              delete interaction.residues_2;
-              delete interaction.interface_1;
-              delete interaction.interface_2;
-            }
+        if (!retrieved) return response.end();
+        const metadata = retrieved.metadata;
+        if (!metadata) return response.json({ error: 'There is no metadata' });
+        // Prepare the input interactions as only interaction names and selections
+        const interactions = metadata.INTERACTIONS;
+        if (interactions) {
+          for (const interaction of interactions) {
+            delete interaction.residues_1;
+            delete interaction.residues_2;
+            delete interaction.interface_1;
+            delete interaction.interface_2;
           }
-          // Prepare the inputs json file to be sent
-          if (metadata) {
-            const inputs = {
-              chainnames: metadata.CHAINNAMES,
-              ligands: metadata.LIGANDS,
-              domains: metadata.DOMAINS,
-              interactions: interactions,
-              forced_references: metadata.FORCED_REFERENCES,
-              pdbIds: metadata.PDBIDS,
-              name: metadata.NAME,
-              description: metadata.DESCRIPTION,
-              contact: metadata.CONTACT,
-              authors: metadata.AUTHORS,
-              groups: metadata.GROUPS,
-              program: metadata.PROGRAM,
-              version: metadata.VERSION,
-              type: metadata.TYPE,
-              method: metadata.METHOD,
-              links: metadata.LINKS,
-              license: metadata.LICENSE,
-              linkcense: metadata.LINKCENSE,
-              citation: metadata.CITATION,
-              thanks: metadata.THANKS,
-              length: metadata.LENGTH,
-              temp: metadata.TEMP,
-              ensemble: metadata.ENSEMBLE,
-              timestep: metadata.TIMESTEP,
-              ff: metadata.FF,
-              wat: metadata.WAT,
-              boxtype: metadata.BOXTYPE,
-              exceptions: metadata.EXCEPTIONS,
-              membranes: metadata.MEMBRANES,
-              customs: metadata.CUSTOMS,
-              orientation: metadata.ORIENTATION,
-              collections: metadata.COLLECTIONS,
-              cv19_unit: metadata.CV19_UNIT,
-            };
-            response.json(inputs);
-          } else response.json({ error: 'There is no metadata' });
-        } else response.end();
+        }
+        // Prepare the inputs json file to be sent
+        const inputs = {
+          chainnames: metadata.CHAINNAMES,
+          ligands: metadata.LIGANDS,
+          domains: metadata.DOMAINS,
+          interactions: interactions,
+          pbc_selection: metadata.PBC_SELECTION,
+          forced_references: metadata.FORCED_REFERENCES,
+          pdbIds: metadata.PDBIDS,
+          name: metadata.NAME,
+          description: metadata.DESCRIPTION,
+          contact: metadata.CONTACT,
+          authors: metadata.AUTHORS,
+          groups: metadata.GROUPS,
+          program: metadata.PROGRAM,
+          version: metadata.VERSION,
+          type: metadata.TYPE,
+          method: metadata.METHOD,
+          links: metadata.LINKS,
+          license: metadata.LICENSE,
+          linkcense: metadata.LINKCENSE,
+          citation: metadata.CITATION,
+          thanks: metadata.THANKS,
+          length: metadata.LENGTH,
+          temp: metadata.TEMP,
+          ensemble: metadata.ENSEMBLE,
+          timestep: metadata.TIMESTEP,
+          ff: metadata.FF,
+          wat: metadata.WAT,
+          boxtype: metadata.BOXTYPE,
+          exceptions: metadata.EXCEPTIONS,
+          membranes: metadata.MEMBRANES,
+          customs: metadata.CUSTOMS,
+          orientation: metadata.ORIENTATION,
+          collections: metadata.COLLECTIONS,
+        };
+        // Add collection specific fields
+        if (metadata.COLLECTIONS == 'cv19') {
+          inputs.cv19_unit = metadata.CV19_UNIT;
+        }
+        // WARNING: Note that parsing to json makes disappear all fields set as 'undefined'
+        response.json(inputs);
       },
     }),
   );
