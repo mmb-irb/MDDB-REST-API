ce5427da4fd9e3604abf75e33bcf31e7fa2c9204
dbeltran <daniel.beltran@irbbarcelona.org>
2023-09-07 19:51:31 +0200

    support options for references queries

--- src/routes/projects/options/index.js
+++ src/routes/projects/options/index.js
@@ -9,7 +9,7 @@ const getBaseFilter = require('../../../utils/base-filter');
 const analysisRouter = Router({ mergeParams: true });
 
 // This endpoint returns some options of data contained in the projects collection
-module.exports = (_, { projects }) => {
+module.exports = (_, { projects, references }) => {
   // Root
   analysisRouter.route('/').get(
     handler({
@@ -19,8 +19,81 @@ module.exports = (_, { projects }) => {
         if (!projection) return { error: 'Missing projection' };
         if (typeof projection === 'string') projection = [projection];
         // Set the projection object for the mongo query
-        const projector = {};
+        const projector = { _id: false, uniprot: true };
         projection.forEach(p => (projector[p] = true));
+        // Set the options object to be returned
+        // Then all mined data will be written into it
+        const options = {};
+        // Options may be querying the projects collection (default) or the references collection
+        // If the 'ref' flag is passed (even empty) then do the query against references
+        if (request.query.ref !== undefined) {
+          // Get all project references to be used further
+          const projectsCursor = await projects.find(
+            getBaseFilter(request),
+            // Discard the heaviest fields we do not need anyway
+            { projection: { 'metadata.REFERENCES': true, _id: false } },
+          );
+          // Consume the projects cursor and keep only the references
+          const projectReferences = await projectsCursor
+            .map(project => project.metadata.REFERENCES)
+            .toArray();
+          // Get all references
+          const referencesCursor = await references.find(
+            getBaseFilter(request),
+            // Discard the heaviest fields we do not need anyway
+            { projection: projector },
+          );
+          // Consume the references cursor
+          const data = await referencesCursor.toArray();
+          // For each projected field, get the different available values and the uniprot ids of references which contain them
+          projection.forEach(field => {
+            const values = {};
+            // Set a function to mine values
+            const getValues = (object, steps, uniprot_id) => {
+              let value = object;
+              for (const [index, step] of steps.entries()) {
+                // Get the actual value
+                value = value[step];
+                if (value === undefined) return;
+                // In case it is an array search for the remaining steps on each element
+                if (Array.isArray(value)) {
+                  const remainingSteps = steps.slice(index + 1);
+                  value.forEach(element =>
+                    getValues(element, remainingSteps, uniprot_id),
+                  );
+                  return;
+                }
+              }
+              // If the value exists and it is not an array then add it to the list
+              // First create an empty list in case this is the first time we find this value
+              if (!values[value]) values[value] = [];
+              // Then add the uniprot id to the list
+              values[value].push(uniprot_id);
+            };
+            // Run the actual values mining
+            const fieldSteps = field.split('.');
+            data.forEach(reference =>
+              getValues(reference, fieldSteps, reference.uniprot),
+            );
+            // Now, for each value, convert the uniprot ids list in the count of projects including any of these uniprot ids
+            const counts = {};
+            Object.entries(values).forEach(([value, uniprot_ids]) => {
+              let count = 0;
+              for (const references of projectReferences) {
+                if (references === null) continue;
+                if (
+                  references.some(reference => uniprot_ids.includes(reference))
+                )
+                  count += 1;
+              }
+              counts[value] = count;
+            });
+            // Add current field counts to the options object to be returned
+            options[field] = counts;
+          });
+          return options;
+        }
+        // In case we are querying the projects collection
         // Get all projects
         const cursor = await projects.find(
           getBaseFilter(request),
@@ -29,9 +102,6 @@ module.exports = (_, { projects }) => {
         );
         // Consume the cursor
         const data = await cursor.toArray();
-        // Set the options object to be returned
-        // Then all mined data will be written into it
-        const options = {};
         // For each projected field, get the counts
         projection.forEach(field => {
           const values = [];
