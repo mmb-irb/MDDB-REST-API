cac5bbfa2c459617a438fd0508467cf8b4d2d849
dbeltran <daniel.beltran@irbbarcelona.org>
2023-09-26 18:56:53 +0200

    summary endpoint now accepts a query

--- src/routes/projects/summary/index.js
+++ src/routes/projects/summary/index.js
@@ -8,15 +8,84 @@ const { getBaseFilter } = require('../../../utils/get-project-query');
 
 const analysisRouter = Router({ mergeParams: true });
 
+// Set a header for queried fields to be queried in the references collection instead of projects
+const referencesHeader = 'references.';
+
+// Try to parse JSON and return the bad request error in case it fails
+const parseJSON = string => {
+  try {
+    const parse = JSON.parse(string);
+    if (parse && typeof parse === 'object') return parse;
+  } catch (e) {
+    return false;
+  }
+};
+
 // This endpoint returns some summary of data contained in the projects collection
 module.exports = (_, { projects }) => {
   // Root
   analysisRouter.route('/').get(
     handler({
       async retriever(request) {
+        // Set an object with all the parameters to performe the mongo query
+        // Start filtering by published projects only if we are in production environment
+        const finder = getBaseFilter(request);
+        // Handle when there is a mongo query
+        let query = request.query.query;
+        if (query) {
+          // In case there is a single query it would be a string, not an array, so adapt it
+          if (typeof query === 'string') query = [query];
+          for (const q of query) {
+            // Parse the string into an object
+            const projectsQuery = parseJSON(q);
+            if (!projectsQuery) return {
+              headerError: BAD_REQUEST,
+              error: 'Wrong query syntax: ' + q
+            };
+            // At this point the query object should correspond to a mongo query itself
+            // Find fields which start with 'references'
+            // These fields are actually intended to query the references collections
+            // If we found references fields then we must query the references collection
+            // Then each references field will be replaced by a query to 'metadata.REFERENCES' in the projects query
+            // The value of 'metadata.REFERENCES' to be queried will be the matching uniprot ids
+            const parseReferencesQuery = async original_query => {
+              // Iterate over the original query fields
+              for (const [field, value] of Object.entries(original_query)) {
+                // If the field is actually a list of fields then run the parsing function recursively
+                if (field === '$and' || field === '$or') {
+                  for (const subquery of value) {
+                    await parseReferencesQuery(subquery);
+                  }
+                  return;
+                }
+                // If the field does not start with the references header then skip it
+                if (!field.startsWith(referencesHeader)) return;
+                // Get the name of the field after substracting the symbolic header
+                const referencesField = field.replace(referencesHeader, '');
+                const referencesQuery = {};
+                referencesQuery[referencesField] = value;
+                // Query the references collection
+                // WARNING: If the query is wrong it will not make the code fail until the cursor in consumed
+                const referencesCursor = await model.references
+                  .find(referencesQuery)
+                  .project({ uniprot: true, _id: false });
+                const results = await referencesCursor
+                  .map(ref => ref.uniprot)
+                  .toArray();
+                // Update the original query by removing the original field and adding the parsed one
+                delete original_query[field];
+                original_query['metadata.REFERENCES'] = { $in: results };
+              }
+            };
+            // Start the parsing function
+            await parseReferencesQuery(projectsQuery);
+            if (!finder.$and) finder.$and = [];
+            finder.$and.push(projectsQuery);
+          }
+        }
         // Get all projects
         const cursor = await projects.find(
-          getBaseFilter(request),
+          finder,
           // Discard the heaviest fields we do not need anyway
           {
             projection: {
