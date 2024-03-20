c7ddfc1f1fc6b697a093a44223662443ff6b9306
dbeltran <daniel.beltran@irbbarcelona.org>
2024-03-20 16:51:25 +0100

    inputs endpoint supports yaml and fields resorted

--- src/routes/projects/inputs/index.js
+++ src/routes/projects/inputs/index.js
@@ -2,33 +2,69 @@ const Router = require('express').Router;
 
 const handler = require('../../../utils/generic-handler');
 
-const { NOT_FOUND } = require('../../../utils/status-codes');
+const { NOT_FOUND, BAD_REQUEST } = require('../../../utils/status-codes');
 // Get an automatic mongo query parser based on environment and request
 const { getProjectQuery } = require('../../../utils/get-project-query');
 
+// Import yaml parsing tool
+const yaml = require('yamljs');
+
 const analysisRouter = Router({ mergeParams: true });
+
+// Set the accepted formats and all their accepted names
+const supportedFormats = {
+  yaml: 'yaml',
+  yml: 'yaml',
+  json: 'json'
+};
+
+// Set the defualt format for the inputs file
+const defaultFormat = 'yaml';
 
 // This endpoint builds the MoDEL workflow's 'inputs.json' file
 module.exports = (_, { projects }) => {
   // Root
   analysisRouter.route('/').get(
     handler({
-      retriever(request) {
-        // Return the project which matches the request accession
-        return projects.findOne(
+      async retriever(request) {
+        // Set the format to export inputs
+        let format = defaultFormat;
+        // Get the requested format
+        const queryFormat = request.query.format;
+        if (queryFormat) {
+          const requestedFormat = supportedFormats[queryFormat];
+          // If requested format is not defined then return an error
+          if (!requestedFormat) {
+            const availableFormats = [...new Set(Object.values(supportedFormats))].join(', ');
+            return {
+              headerError: BAD_REQUEST,
+              error: `Query format "${queryFormat}" is not supported. Please ask for one of these: ${availableFormats}`
+            };
+          }
+          format = requestedFormat;
+        }
+        // Get the requested project data
+        const projectData = await projects.findOne(
           getProjectQuery(request),
           // But return only the "metadata" attribute
           { projection: { _id: false, metadata: true, mds: true, mdref: true } },
         );
+        // Return the project which matches the request accession
+        return { projectData, format }
       },
       // If there is nothing retrieved send a NOT_FOUND status in the header
       headers(response, retrieved) {
         if (!retrieved) response.sendStatus(NOT_FOUND);
+        // In case of error
+        if (retrieved.headerError) return response.status(retrieved.headerError);
       },
       // If there is retrieved and the retrieved has metadata then send the inputs file
       body(response, retrieved) {
         if (!retrieved) return response.end();
-        const metadata = retrieved.metadata;
+        // In case of error
+        if (retrieved.error) return response.json(retrieved.error);
+        const projectData = retrieved.projectData;
+        const metadata = projectData.metadata;
         if (!metadata) return response.json({ error: 'There is no metadata' });
         // Prepare the input interactions as only interaction names and selections
         const interactions = metadata.INTERACTIONS;
@@ -41,7 +77,7 @@ module.exports = (_, { projects }) => {
           }
         }
         // Set the input mds by removing all generated fields on each MD
-        retrieved.mds.forEach(md => {
+        projectData.mds.forEach(md => {
           delete md.atoms;
           delete md.frames;
           delete md.analyses;
@@ -50,42 +86,40 @@ module.exports = (_, { projects }) => {
         })
         // Prepare the inputs json file to be sent
         const inputs = {
-          chainnames: metadata.CHAINNAMES,
-          ligands: metadata.LIGANDS,
-          domains: metadata.DOMAINS,
-          interactions: interactions,
-          pbc_selection: metadata.PBC_SELECTION,
-          forced_references: metadata.FORCED_REFERENCES,
-          pdbIds: metadata.PDBIDS,
           name: metadata.NAME,
           description: metadata.DESCRIPTION,
-          contact: metadata.CONTACT,
           authors: metadata.AUTHORS,
           groups: metadata.GROUPS,
+          contact: metadata.CONTACT,
           program: metadata.PROGRAM,
           version: metadata.VERSION,
           type: metadata.TYPE,
           method: metadata.METHOD,
-          links: metadata.LINKS,
           license: metadata.LICENSE,
           linkcense: metadata.LINKCENSE,
           citation: metadata.CITATION,
           thanks: metadata.THANKS,
-          length: metadata.LENGTH,
+          links: metadata.LINKS,
+          pdbIds: metadata.PDBIDS,
+          framestep: metadata.FRAMESTEP,
           temp: metadata.TEMP,
           ensemble: metadata.ENSEMBLE,
           timestep: metadata.TIMESTEP,
           ff: metadata.FF,
           wat: metadata.WAT,
           boxtype: metadata.BOXTYPE,
-          exceptions: metadata.EXCEPTIONS,
+          interactions: interactions,
+          pbc_selection: metadata.PBC_SELECTION,
+          forced_references: metadata.FORCED_REFERENCES,
+          multimeric: metadata.MULTIMERIC,
+          chainnames: metadata.CHAINNAMES,
+          ligands: metadata.LIGANDS,
           membranes: metadata.MEMBRANES,
           customs: metadata.CUSTOMS,
           orientation: metadata.ORIENTATION,
           collections: metadata.COLLECTIONS,
-          multimeric: metadata.MULTIMERIC,
-          mds: retrieved.mds,
-          mdref: retrieved.mdref,
+          mds: projectData.mds,
+          mdref: projectData.mdref,
           // Input file paths are written to the json file for coherence
           // However they are left as none since the workflow will use defaults
           input_structure_filepath: null,
@@ -100,7 +134,10 @@ module.exports = (_, { projects }) => {
           inputs.cv19_nanobs = metadata.CV19_NANOBS;
         }
         // WARNING: Note that parsing to json makes disappear all fields set as 'undefined'
-        response.json(inputs);
+        if (retrieved.format === 'json') response.json(inputs);
+        // WARNING: Note that the YAML file has no comments as when it is generated from the workflow
+        else if (retrieved.format === 'yaml') response.end(yaml.stringify(inputs));
+        else throw new Error(`Format not supported ${retrieved.format}`);
       },
     }),
   );
