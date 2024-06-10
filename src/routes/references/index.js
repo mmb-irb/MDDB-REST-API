8a9d68b5f22cb5e71e9801c3b35ad635dce1889d
dbeltran <daniel.beltran@irbbarcelona.org>
2024-06-10 10:56:51 +0200

    more standarized references endpoint

--- src/routes/references/index.js
+++ src/routes/references/index.js
@@ -1,26 +1,20 @@
 const rootRouter = require('express').Router();
 // API generic handler
 const handler = require('../../utils/generic-handler');
+// Get the database handler
+const getDatabase = require('../../database');
 // Import references configuration
 const { REFERENCES } = require('../../utils/constants');
 const availableReferences = Object.keys(REFERENCES).join(', ');
 // Standard codes for HTTP responses
 const { NOT_FOUND, INTERNAL_SERVER_ERROR } = require('../../utils/status-codes');
 
-// Connect to the mongo database and return the connection
-// Alternatively, in 'test' context, connect to a local fake mongo database and return the connection
-const dbConnection = process.env.NODE_ENV === 'test'
-    ? require('../../../test-helpers/mongo/index')
-    : require('../../models/index');
-
 // Set the response when a specific reference is requested
 // Return a list with all available reference ids
 const wholeReferenceResponse = handler({
     async retriever(request) {
-        // Save the mongo database connection
-        const client = await dbConnection;
-        // Access the database
-        const db = client.db(process.env.DB_NAME);
+        // Stablish database connection and retrieve our custom handler
+        const database = await getDatabase(request);
         // Get the requested reference configuration
         const referenceName = request.params.reference;
         const reference = REFERENCES[referenceName];
@@ -29,7 +23,7 @@ const wholeReferenceResponse = handler({
             error: `Unknown reference "${referenceName}". Available references: ${availableReferences}`
         };
         // Set the target mongo collection
-        const collection = db.collection(reference.collectionName);
+        const collection = database[referenceName];
         // Get all references, but only their reference ids
         const cursor = await collection.find({},
             { projection: { _id: false, [reference.idField]: true } },
@@ -39,32 +33,15 @@ const wholeReferenceResponse = handler({
         // Get the reference ids in an array
         const referenceIds = references.map(ref => ref[reference.idField]);
         return referenceIds;
-    },
-    headers(response, retrieved) {
-        // There should always be a retrieved object
-        if (!retrieved) return response.sendStatus(INTERNAL_SERVER_ERROR);
-        // If there is any specific header error in the retrieved then send it
-        if (retrieved.headerError) response.status(retrieved.headerError);
-    },
-    body(response, retrieved) {
-        // If nothing is retrieved then end the response
-        // Note that the header 'sendStatus' function should end the response already, but just in case
-        if (!retrieved) return response.end();
-        // If there is any error in the body then just send the error
-        if (retrieved.error) return response.json(retrieved.error);
-        // Send the response
-        response.json(retrieved);
-    },
+    }
 });
 
 // Set the response when a specific reference id is requested
 // Return an the requested reference object
 const specificReferenceResponse = handler({
     async retriever(request) {
-        // Save the mongo database connection
-        const client = await dbConnection;
-        // Access the database
-        const db = client.db(process.env.DB_NAME);
+        // Stablish database connection and retrieve our custom handler
+        const database = await getDatabase(request);
         // Get the requested reference configuration
         const referenceName = request.params.reference;
         const reference = REFERENCES[referenceName];
@@ -73,7 +50,7 @@ const specificReferenceResponse = handler({
             error: `Unknown reference "${referenceName}". Available references: ${availableReferences}`
         };
         // Set the target mongo collection
-        const collection = db.collection(reference.collectionName);
+        const collection = database[referenceName];
         // Get the requested id
         const referenceId = request.params.id;
         // Return the reference which matches the request id
@@ -89,24 +66,7 @@ const specificReferenceResponse = handler({
             error: `Not found "${referenceName}" reference with id "${referenceId}"`
         };
         return result;
-    },
-    // If no project is found, a NOT_FOUND status is sent in the header
-    headers(response, retrieved) {
-        // There should always be a retrieved object
-        if (!retrieved) return response.sendStatus(INTERNAL_SERVER_ERROR);
-        // If there is any specific header error in the retrieved then send it
-        if (retrieved.headerError) response.status(retrieved.headerError);
-    },
-    // Else, the project object is cleaned (some attributes are renamed or removed) and sent in the body
-    body(response, retrieved) {
-        // If nothing is retrieved then end the response
-        // Note that the header 'sendStatus' function should end the response already, but just in case
-        if (!retrieved) return response.end();
-        // If there is any error in the body then just send the error
-        if (retrieved.error) return response.json(retrieved.error);
-        // Send the response
-        response.json(retrieved);
-    },
+    }
 });
 
 // Set the routing
