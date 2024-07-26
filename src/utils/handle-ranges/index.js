86679a8918a52a2166edf5e391256d81384f67ea
dbeltran <daniel.beltran@irbbarcelona.org>
2024-07-26 18:08:36 +0200

    yet another fix over the previous bug

--- src/utils/handle-ranges/index.js
+++ src/utils/handle-ranges/index.js
@@ -168,7 +168,10 @@ const handleRanges = (request, parsedRanges, descriptor) => {
 
   // In case all ranges are whole we return the range now that it is not yet iterable
   // This will make the trajectory stream not ranged at all
-  if (dimensions.every(dim => range[dim].whole)) return range;
+  if (dimensions.every(dim => range[dim].whole)) {
+    range.byteSize = descriptor.length;
+    return range;
+  }
   
   // Here we're about to generate an array with the combinations of all dimensions, which might be A LOT!
   // In order to save memory we generate them on the fly
